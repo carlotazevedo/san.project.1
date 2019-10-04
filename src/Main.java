4db58ea4ed138b608a260fab9595ed31fc7d76b7
carlota_barbosa <carlota_azevedo@hotmail.com>
2019-10-04 23:23:35 +0200

    first complete exercice

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to my first project!");
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Welcome to my first project San!");
+
+        System.out.println("Choose your options!");
+
+        System.out.println("[1]");
+        System.out.println("[2]");
+        System.out.println("[3]");
+        System.out.println("[4]");
+
+
+        int imput = s.nextInt();
+
+        if (imput == 1) {
+            System.out.println("Kw0n arms are broken");
+
+        }
+
+        else if (imput ==2){
+            System.out.println("Crim is secretly pro Doomfist");
+        }
+
+        else if (imput ==3){
+            System.out.println("I’m gay with plants");
+
+        }
+
+        else if (imput==4){
+            System.out.println("San is the best teacher");
+
+        }
+        else{
+            System.out.println("Invalide Choice");
+
+        }
+
+
+
 
 
     }
