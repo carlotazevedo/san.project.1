2367853755fd8359f746a2268c14352b079eea0b
carlota_barbosa <carlota_azevedo@hotmail.com>
2019-10-05 01:17:09 +0200

    first complete exercice

--- src/Main.java
+++ src/Main.java
@@ -4,44 +4,54 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to my first project San!");
 
         System.out.println("Choose your options!");
 
-        System.out.println("[1]");
-        System.out.println("[2]");
-        System.out.println("[3]");
-        System.out.println("[4]");
+        int input;
 
 
-        int imput = s.nextInt();
+        do {
+            System.out.println("[1]");
+            System.out.println("[2]");
+            System.out.println("[3]");
+            System.out.println("[4]");
 
-        if (imput == 1) {
-            System.out.println("Kw0n arms are broken");
-
-        }
-
-        else if (imput ==2){
-            System.out.println("Crim is secretly pro Doomfist");
-        }
-
-        else if (imput ==3){
-            System.out.println("I’m gay with plants");
-
-        }
-
-        else if (imput==4){
-            System.out.println("San is the best teacher");
-
-        }
-        else{
-            System.out.println("Invalide Choice");
-
-        }
+            input = scanner.nextInt();
 
 
+
+            if (input == 1) {
+                System.out.println("Kw0n arms are broken");
+
+            }
+
+            else if (input ==2){
+                System.out.println("Crim is secretly pro Doomfist");
+            }
+
+            else if (input ==3){
+                System.out.println("I’m gay with plants");
+
+            }
+
+            else if (input==4){
+                System.out.println("San is the best teacher");
+
+            }
+
+            else if (input ==0){
+                System.out.println("bai bai!");
+            }
+            else{
+                System.out.println("Invalide Choice");
+
+            }
+
+
+        } while (input !=0);
 
 
 
