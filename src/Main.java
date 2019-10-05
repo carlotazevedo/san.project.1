5a1dc3659e28094cd21951ab1a60d0723a5a32d6
Sanhora <dev@rumley.ch>
2019-10-05 16:19:56 +0200

    Changed the "if" statements with "switch/case" The "if" statements are commented just so you can compare.

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,9 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to my first project San!");
-
         System.out.println("Choose your options!");
 
         int input;
-
 
         do {
             System.out.println("[1]");
@@ -21,39 +19,46 @@ public class Main {
 
             input = scanner.nextInt();
 
+            switch (input) {
+                case 1:
+                    System.out.println("Kw0n arms are broken");
+                    break;
+                case 2:
+                    System.out.println("Crim is secretly pro Doomfist");
+                    break;
+                case 3:
+                    System.out.println("I’m gay with plants");
+                    break;
+                case 4:
+                    System.out.println("San is the best teacher");
+                    break;
+                case 0:
+                    System.out.println("bai bai!");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
 
-
+            /*
             if (input == 1) {
                 System.out.println("Kw0n arms are broken");
 
-            }
-
-            else if (input ==2){
+            } else if (input == 2) {
                 System.out.println("Crim is secretly pro Doomfist");
-            }
-
-            else if (input ==3){
+            } else if (input == 3) {
                 System.out.println("I’m gay with plants");
 
-            }
-
-            else if (input==4){
+            } else if (input == 4) {
                 System.out.println("San is the best teacher");
 
-            }
-
-            else if (input ==0){
+            } else if (input == 0) {
                 System.out.println("bai bai!");
-            }
-            else{
+            } else {
                 System.out.println("Invalide Choice");
+            }*/
 
-            }
-
-
-        } while (input !=0);
-
-
+        } while (input != 0);
 
     }
 }
